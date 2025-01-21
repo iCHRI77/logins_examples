bf1de354e35818eadd82c6d74cc52183aa1d051b
iCHRI77 <christianjbarbosab@gmail.com>
2025-01-21 14:37:30 -0500

    fix($nav): change the list, now is in nav tag

--- app/page.tsx
+++ app/page.tsx
@@ -17,43 +17,37 @@ export default function Home() {
         <h1 className="text-3xl font-bold text-[#FFFFFF]">Change the view: </h1>
         <div className="justify-self-center text-center mt-4">
 
-          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto gap-6">
-            <li key={1} className="group/item bg-[#3f967c] hover:bg-[#353535] rounded-md">
-              <button onClick={() => {
-                setView("basicLogin")
-                setShowReturn(true)
-              }}
-                className="text-md px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75" >
-                <a href="#loginView">Basic login</a>
-              </button>
-            </li>
-            <li key={2} className="group/item bg-[#3f967c] hover:bg-[#353535] rounded-md">
-              <button onClick={() => {
-                setView("boxedLogin")
-                setShowReturn(true)
-              }}
-                className="text-md px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75" >
-                <a href="#loginView">Boxed login</a>
-              </button>
-            </li>
-            <li key={3} className="group/item bg-[#3f967c] hover:bg-[#353535] rounded-md">
-              <button onClick={() => {
-                setView("boxedStyledLogin")
-                setShowReturn(true)
-              }}
-                className="text-md px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75" >
-                <a href="#loginView">Boxed styled login</a>
-              </button>
-            </li>
-          </ul>
+          <nav>
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto gap-6">
+              <li>
+                <a href="#loginView" onClick={() => {
+                  setView("basicLogin")
+                  setShowReturn(true)
+                }} className="text-md px-3 py-2 flex items-center uppercase font-bold leading-snug bg-[#3f967c] text-white hover:opacity-75">Basic login</a>
+              </li>
+              <li>
+                <a href="#loginView" onClick={() => {
+                  setView("boxedLogin")
+                  setShowReturn(true)
+                }} className="text-md px-3 py-2 flex items-center uppercase font-bold leading-snug bg-[#3f967c] text-white hover:opacity-75">Boxed login</a>
+              </li>
+              <li >
+                <a href="#loginView" onClick={() => {
+                  setView("boxedStyledLogin")
+                  setShowReturn(true)
+                }} className="text-md px-3 py-2 flex items-center uppercase font-bold leading-snug bg-[#3f967c] text-white hover:opacity-75">Boxed styled login</a>
+              </li>
+            </ul>
+          </nav>
+
           <p className="mt-4 text-white">Click one of the above to watch new login page</p>
         </div>
       </div>
-      {showReturn == true &&
-        <button onClick={() => setShowReturn(false)} className="fixed bg-[#3f967c] hover:bg-[#353535] text-white font-bold py-2 px-4 rounded-md mt-4" >
-          <a href="#buttons">Return</a>
-        </button>
+
+      {showReturn === true &&
+        <a href="#buttons" className="flex justify-center fixed top-20 left-1/2 -translate-x-1/2  bg-[#3f967c] hover:bg-[#353535] text-white font-bold py-2 px-4 rounded-md mt-4" onClick={() => setShowReturn(false)} >Return</a>
       }
+
       {view === "basicLogin" && <BasicLogin />}
       {view === "boxedLogin" && <BoxedLogin />}
       {view === "boxedStyledLogin" && <BoxedStyledLogin />}
